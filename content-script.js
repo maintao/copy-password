9732128af900f6b6aebfeb7e25793b458953996e
maintao <dongchao@outlook.com>
2022-09-07 22:58:48 +0800

    add http:// support

--- content-script.js
+++ content-script.js
@@ -3,6 +3,38 @@
     return /(Mac|iPhone|iPod|iPad)/i.test(navigator.userAgent);
   }
 
+  function writeTextToClipboard(text) {
+    // navigator clipboard api needs a secure context (https)
+    if (navigator.clipboard && window.isSecureContext) {
+      return navigator.clipboard.writeText(text);
+    } else {
+      let currentFocusElement = document.activeElement;
+      let currentSelectionText = window.getSelection().toString();
+
+      // make temporary textarea element
+      let textArea = document.createElement("textarea");
+      textArea.value = text;
+      // the textarea have to be on page and technically visible
+      textArea.style.position = "fixed";
+      textArea.style.width = "0";
+      textArea.style.height = "0";
+      textArea.style.padding = "0";
+      textArea.style.border = "none";
+      document.body.appendChild(textArea);
+      textArea.select(); // select text, get ready to copy
+
+      return new Promise((resolve, reject) => {
+        // execute copy command, though this api is deprecated
+        document.execCommand("copy") ? resolve() : reject();
+        // do the cleanup
+        textArea.remove();
+        if (currentSelectionText.length > 0) {
+          currentFocusElement.select();
+        }
+      });
+    }
+  }
+
   let copyModifierKey = isMac() ? "Meta" : "Control";
   let isCopyModifierKeyDown = false;
 
@@ -11,7 +43,7 @@
       isCopyModifierKeyDown = true;
     } else if (isCopyModifierKeyDown && event.key === "c") {
       if (document.activeElement.type === "password" && document.activeElement.value !== "") {
-        navigator.clipboard.writeText(document.activeElement.value);
+        writeTextToClipboard(document.activeElement.value);
       }
     }
   });
